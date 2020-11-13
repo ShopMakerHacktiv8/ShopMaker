80204649be8e7078c2b3a360c8926fea784b56dd
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-11-13 23:20:46 +0700

    TDD Product

--- server_paten/controllers/productController.js
+++ server_paten/controllers/productController.js
@@ -1,62 +1,37 @@
 const { Product, Category } = require('../models')
 
 class ProductController {
-  static createProduct(req, res, next) {
-    // try {
-    //   const { shop_id, category_id, name, image_url, price, stock, description } = req.body
-    //   const productObj = { shop_id, category_id, name, image_url, price, stock, description }
-    //   console.log(productObj, '<=== product obj')
-    //   const newProduct = await Product.create({ productObj })
-    //   console.log(newProduct, '<=== new product')
+  static async createProduct(req, res, next) {
+    try {
+      const { category_id, name, price, stock, description, file } = req.body
+      const productObj = { shop_id: req.shopData.id, category_id, name, image_url: file, price, stock, description }
+      const newProduct = await Product.create(productObj)
 
-    //   res.status(201).json({
-    //     id: newProduct.id,
-    //     shop_id: newProduct.shop_id,
-    //     category_id: newProduct.category_id,
-    //     name: newProduct.name,
-    //     image_url: newProduct.image_url,
-    //     price: newProduct.price,
-    //     stock: newProduct.stock,
-    //     description: newProduct.description
-    //   });
+      res.status(201).json({
+        id: newProduct.id,
+        shop_id: newProduct.shop_id,
+        category_id: newProduct.category_id,
+        name: newProduct.name,
+        image_url: newProduct.image_url,
+        price: newProduct.price,
+        stock: newProduct.stock,
+        description: newProduct.description
+      });
             
-    // } catch (err) {
-    //   console.log(err, '<==== error create product at controller')
-    //   next(err)
-    // }
-    const { shop_id, category_id, name, image_url, price, stock, description } = req.body
-    // console.log(req.body, '<=== req.body at create controller')
-
-    Product.create({
-      shop_id, category_id, name, image_url, price, stock, description
-    })
-      .then(product => {
-        // console.log(product, '<=== create product di controller')
-        res.status(201).json({
-          id: product.id,
-          shop_id: product.shop_id,
-          category_id: product.category_id,
-          name: product.name,
-          image_url: product.image_url,
-          price: product.price,
-          stock: product.stock,
-          description: product.description
-        })
-      })
-      .catch(err => {
-        console.log(err, '<==== error create product di controller')
-        next(err)
-      })
+    } catch (err) {
+      next(err)
+    }
   }
 
   static async getProduct(req, res, next) {
     try {
       const { category_id } = req.query
+      const { shop_id } = req.body
       let products = []
       if (category_id) {
         products = await Product.findAll({
           order: [["id", "DESC"]],
-          where: { category_id },
+          where: { category_id, shop_id },
           include: {
             model: Category,
             attributes: {
@@ -67,6 +42,7 @@ class ProductController {
       } else {
         products = await Product.findAll({
           order: [["id", "DESC"]],
+          where: { shop_id },
           include: {
             model: Category,
             attributes: {
@@ -89,7 +65,6 @@ class ProductController {
       });
       res.status(200).json(products)
     } catch (err) {
-      console.log(err, '<=== error get all product at controller')
       next(err)
     }
   }
@@ -98,11 +73,10 @@ class ProductController {
     try {
       const { id } = req.params
       const product = await Product.findByPk(id)
-      if (!product) throw { msg: 'product not found!', statusCode: 404}
+      if (!product) throw { msg: 'product not found', statusCode: 404}
 
       res.status(200).json(product)      
     } catch (err) {
-      console.log(err, '<=== error get by id product at controller')
       next(err)
     }
   }
@@ -111,70 +85,47 @@ class ProductController {
     try {
       const { id } = req.params
       const product = await Product.findByPk(id)
-      if (!product) throw { msg: 'product not found!', statusCode: 404}
+      if (!product) throw { msg: 'product not found', statusCode: 404}
 
       product.destroy()
       res.status(200).json({
         message: 'delete product success'
       })
     } catch (err) {
-      console.log(err, '<=== error at delete product controller')
       next(err)
     }
   }
 
-  static updateProduct(req, res, next) {
-    const { id } = req.params
-    Product.findByPk(id)
-      .then(product => {
-        console.log(product, '<=== product at update controller')
-        if (!product) throw { err: 'product nor found', statusCode: 404} 
-        return product.update({
-          ...product,
-          ...req.body
-        })
+  static async updateProduct(req, res, next) {
+    try {
+      const { id } = req.params
+      const { name, price, stock, description, category_id, file } = req.body
+      let product = await Product.findByPk(id)
+      if (!product) throw { msg: 'product not found', statusCode: 404}
+
+      product.name = name
+      product.image_url = file
+      product.price = +price
+      product.stock = +stock
+      product.description = description
+      product.category_id = category_id || null;
+
+      await product.save()
+
+      res.status(200).json({
+        id: product.id,
+        name: product.name,
+        image_url: product.image_url,
+        price: product.price,
+        stock: product.stock,
+        description: product.description,
+        category_id: product.category_id
       })
-      .then(product => {
-        console.log(product, '<=== berhasil update di controller product')
-        res.status(200).json(product)
-      })
-      .catch(err => {
-        console.log(err, '<=== error update di product controller')
-        next(err)
-      })
+
+    } catch (err) {
+      next(err)
+    }
   }
-
-  // static async updateProduct(req, res, next) {
-  //   try {
-  //     const { id } = req.params
-  //     const { name, image_url, price, stock, description, category_id } = req.body
-  //     let product = await Product.findByPk(id)
-  //     if (!product) throw { msg: 'product not found!', statusCode: 404}
-
-  //     product.name = name
-  //     product.image_url = image_url
-  //     product.price = price
-  //     product.stock = stock
-  //     product.description = description
-  //     product.category_id = category_id || null;
-
-  //     await Product.save()
-
-  //     res.status(200).json({
-  //       id: product.id,
-  //       name: product.name,
-  //       image_url: product.image_url,
-  //       price: product.price,
-  //       stock: product.stock,
-  //       description: product.description,
-  //       category_id: product.category_id
-  //     })
-
-  //   } catch (err) {
-  //     console.log(err, '<=== error update product at controller')
-  //     next(err)
-  //   }
-  // }
 }
 
 module.exports = ProductController
