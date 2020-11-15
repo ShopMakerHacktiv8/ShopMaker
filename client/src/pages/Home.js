580f51f9a32d35567b68c3f330a9cd666bcffbdd
nabilazora <nabilazora@gmail.com>
2020-11-15 15:17:07 +0700

    cart Customer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Col, Container, Button, Image } from 'react-bootstrap'
+import { Card, Row, Col, Container, Button, Image } from 'react-bootstrap';
 
 function Home() {
   return (
@@ -8,70 +8,69 @@ function Home() {
         <Col xs="6" md="4">
           <Card className="shadow p-4 mt-4">
             <Row className="col-12 justify-content-center">
-              <Image src={"./assets/macbook.jpg"}></Image>
+              <Image src={'./assets/macbook.jpg'}></Image>
             </Row>
             <Row className="col-12 justify-content-center">
-              <h5>Macbook</h5>        
-            </Row>
-            <Row className="col-12 justify-content-center">
-              <h6>Rp 20.000.000</h6>
-            </Row>
-            <Row className="col-12 justify-content-center">
-              <Button class="btn btn-sm btn-outline-info">View</Button>
-            </Row>
-          </Card>
-        </Col>     
-        <Col xs="6" md="4">
-          <Card className="shadow p-4 mt-4" >
-            <Row className="col-12 justify-content-center">
-              <Image src={"./assets/macbook.jpg"}></Image>
-            </Row>
-            <Row className="col-12 justify-content-center">
-              <h5>Macbook</h5>        
+              <h5>Macbook</h5>
             </Row>
             <Row className="col-12 justify-content-center">
               <h6>Rp 20.000.000</h6>
             </Row>
             <Row className="col-12 justify-content-center">
-              <Button class="btn btn-sm btn-outline-info">View</Button>
+              <Button className="btn btn-sm btn-outline-info">View</Button>
             </Row>
           </Card>
-        </Col>  
+        </Col>
         <Col xs="6" md="4">
           <Card className="shadow p-4 mt-4">
             <Row className="col-12 justify-content-center">
-              <Image src={"./assets/macbook.jpg"}></Image>
+              <Image src={'./assets/macbook.jpg'}></Image>
             </Row>
             <Row className="col-12 justify-content-center">
-              <h5>Macbook</h5>        
+              <h5>Macbook</h5>
             </Row>
             <Row className="col-12 justify-content-center">
               <h6>Rp 20.000.000</h6>
             </Row>
             <Row className="col-12 justify-content-center">
-              <Button class="btn btn-sm btn-outline-info">View</Button>
+              <Button className="btn btn-sm btn-outline-info">View</Button>
             </Row>
           </Card>
-        </Col>     
+        </Col>
         <Col xs="6" md="4">
-          <Card className="shadow p-4 mt-4" >
+          <Card className="shadow p-4 mt-4">
             <Row className="col-12 justify-content-center">
-              <Image src={"./assets/macbook.jpg"}></Image>
+              <Image src={'./assets/macbook.jpg'}></Image>
             </Row>
             <Row className="col-12 justify-content-center">
-              <h5>Macbook</h5>        
+              <h5>Macbook</h5>
             </Row>
             <Row className="col-12 justify-content-center">
               <h6>Rp 20.000.000</h6>
             </Row>
             <Row className="col-12 justify-content-center">
-              <Button class="btn btn-sm btn-outline-info">View</Button>
+              <Button className="btn btn-sm btn-outline-info">View</Button>
             </Row>
           </Card>
-        </Col>  
+        </Col>
+        <Col xs="6" md="4">
+          <Card className="shadow p-4 mt-4">
+            <Row className="col-12 justify-content-center">
+              <Image src={'./assets/macbook.jpg'}></Image>
+            </Row>
+            <Row className="col-12 justify-content-center">
+              <h5>Macbook</h5>
+            </Row>
+            <Row className="col-12 justify-content-center">
+              <h6>Rp 20.000.000</h6>
+            </Row>
+            <Row className="col-12 justify-content-center">
+              <Button className="btn btn-sm btn-outline-info">View</Button>
+            </Row>
+          </Card>
+        </Col>
       </Row>
     </Container>
-   
   );
 }
 
