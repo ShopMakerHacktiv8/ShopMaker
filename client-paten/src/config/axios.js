ac42e99f752ebc938aecfe1fb1b41ac22c7c83ea
masterambi <ramzyrash@gmail.com>
2020-11-18 21:05:45 +0700

    deploy firebase

--- client-paten/src/config/axios.js
+++ client-paten/src/config/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-const instance = axios.create({
-  baseURL: 'http://localhost:4000',
-})
+// const instance = axios.create({
+//   baseURL: 'http://localhost:4000',
+// })
 
 // const instance = axios.create({
 //   baseURL: 'https://shopmaker.herokuapp.com',
 // })
 
-// const instance = axios.create({
-//   baseURL: 'https://shopmaker.shopmakerh8.xyz',
-// })
+const instance = axios.create({
+  baseURL: 'https://shopmaker.shopmakerh8.xyz',
+})
 
 export default instance
